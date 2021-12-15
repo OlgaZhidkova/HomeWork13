d6dc234d55fec371182e61c60ea3e3d5582d33dd
Olga Zhidkova <zhidkova.olga12@gmail.com>
2021-12-15 19:45:18 +0300

    Add TabBar and assets

---
+++ HomeWork13/TabBar Controllers/AlbumsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AlbumsViewController.swift
+//  HomeWork13
+//
+//  Created by Ольга on 15.12.2021.
+//
+
+import UIKit
+
+class AlbumsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+        title = "Альбомы"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
---
+++ HomeWork13/TabBar Controllers/ForYouViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ForYouViewController.swift
+//  HomeWork13
+//
+//  Created by Ольга on 15.12.2021.
+//
+
+import UIKit
+
+class ForYouViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        title = "Для Вас"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
